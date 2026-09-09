6244eab1cd1d7729bcd06742a910c035b65e4346
SDETADDA <rmishraqa@gmail.com>
2026-09-09 12:15:10 +0530

    updated workflow and test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,7 +16,7 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
-test('get started link 3', async ({ page }) => {
+test('get started link 4', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
